2fb13452c5616011595801c3a4c3e634b6152cb1
IMRANDIL <aliimranadil2@gmail.com>
2022-03-18 11:47:17 +0530

    yh

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@
 const User = require('../models/userModal')
 
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 exports.userRegister = async (req, res, next) => {
 
 
@@ -50,6 +50,28 @@ exports.userRegister = async (req, res, next) => {
 
 exports.userLogin = async (req, res, next) => {
     try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+
+
+        if (!user) return res.status(400).json({ msg: 'User does not exist!' });
+
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) return res.status(400).json({ msg: 'Invalid Credentials!' })
+
+        //if login success...
+
+        const payload = { id: user._id, name: user.username };
+
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1d' });
+
+
+        res.status(200).json({ token: token, username: user.username })
+
 
     } catch (error) {
         return res.status(500).json({ msg: error.message })
