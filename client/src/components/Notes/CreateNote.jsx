994339d4cbb34e5219d7901566b3d9644c16129d
IMRANDIL <aliimranadil2@gmail.com>
2022-03-20 10:28:22 +0530

    yh

--- client/src/components/Notes/CreateNote.jsx
+++ client/src/components/Notes/CreateNote.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
+
+
 
 
 
 
 
 const CreateNote = () => {
+
+    const [note, setNote] = useState({
+        title: '',
+        content: '',
+        date: ''
+    })
+
+
+
+
+
     return (
-        <div>CreateNote</div>
+        <div className='create-note'>
+            <h2>Create Note</h2>
+
+            <form>
+
+                <div className="row">
+                    <label htmlFor="Title">Title</label>
+                    <input type="text" placeholder='Title' id='Title' value={note.title} name='title' required autoComplete='off' />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="Content">Content</label>
+                    <textarea type="text" rows='10' placeholder='Content' id='Content' value={note.content} name='content' required autoComplete='off' />
+                </div>
+
+                <label htmlFor="Date">Date: {note.date}</label>
+
+                <div className="row">
+
+                    <input type="date" id='Date' name='date' required />
+                </div>
+
+                <button type='submit'>Save</button>
+
+            </form>
+
+        </div>
     )
 }
 
